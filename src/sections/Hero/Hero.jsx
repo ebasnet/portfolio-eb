ecc65194bb252a915438111718f0507edc066120
Eliza <eliza@Elizas-MacBook-Air.local>
2025-03-26 17:02:26 +0545

    changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -45,10 +45,13 @@ function Hero() {
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/ebasnet" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/eliza-b-469590270"
+            target="_blank"
+          >
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
